1a392b97890922f9ba80114d2e64d1d87d371244
Seth <seth.bbrendle@gmail.com>
2022-09-20 15:18:47 -0400

    Lists and Keys Lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
+import ProjectList from "./ProjectList";
 
 function ProjectItem({ name, about, technologies }) {
+
+  const techArray = technologies.map((technology, key) => {
+    return <span key={key}>{technology}</span>
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techArray}
       </div>
     </div>
   );
